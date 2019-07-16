3fb7dd75238d3a52095499115d010dfe99282f6e
jsubcab <jsubcab@gmail.com>
2019-07-16 15:21:14 +0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 const gulp = require("gulp");
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
-gulp.task("default", function() {
-  // code for your default task goes here
+gulp.task("default", ["styles"], function() {
+  gulp.watch("sass/**/*.scss", ["styles"]);
+
+  browserSync.init({
+    server: "./"
+  });
 });
 
 gulp.task("styles", function() {
@@ -16,5 +21,6 @@ gulp.task("styles", function() {
         browsers: ["last 2 versions"]
       })
     )
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 });
